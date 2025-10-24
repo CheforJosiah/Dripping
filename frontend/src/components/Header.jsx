b8ec864638eced7641e535774b59473d38bcccf6
Josiah <josiahchefor@gmail.com>
2025-10-24 15:26:56 +0100

    Adjusting Header, Footer and adding Product page frontend

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,6 @@
-import { Link } from 'react-router';
+import Logo from '../assets/logo.png'
+import MobileLogo from '../assets/mobile-logo.png'
+import CartIcon from '../assets/icons/cart-icon.png'
 import './Header.css'
 
 export function Header() {
@@ -6,24 +8,25 @@ export function Header() {
     <>
       <div className="header">
         <div className="left-section">
-          <Link to="/" className="header-link">
-            <img className="logo" src={logo} alt='Logo' />
-          </Link>
+          <a href="/" className="header-a">
+            <img className="logo" src={Logo} alt='Logo' />
+            <img class="mobile-logo" src={MobileLogo} alt="logo"></img>
+          </a>
         </div>
 
         <div className="middle-section">
-          <Link to="/">Home</Link>
-          <Link to="/products">Shop</Link>
-          <Link to="/pages">Pages</Link>
-          <Link to="/about">About Us</Link>
+          <a href="/">Home</a>
+          <a href="/products">Shop</a>
+          <a href="/pages">Pages</a>
+          <a href="/about">About Us</a>
         </div>
 
         <div className="right-section">
-          <Link className="cart-link header-link" to="/checkout">
-            <img className="cart-icon" src="" alt='Cart' />
+          <a className="cart-link header-link" href="/checkout">
+            <img className="cart-icon" src={CartIcon} alt='Cart' />
             <div className="cart-quantity">1</div>
             <div className="cart-text">Cart</div>
-          </Link>
+          </a>
         </div>
       </div>
     </>
--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -1,24 +1,67 @@
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
+import searchIcon from '../assets/icons/magnifying-glass.png'
+import "./ProductPage.css"
 
 export function ProductPage() {
-
   return (
     <>
       <title>Products</title>
 
       <Header />
+      <div className="product-page">
+        <div className="search">
+          <div>
+            <select className="select">
+              <option value="1">Accessories</option>
+              <option value="2">Laptop</option>
+              <option value="3">Desktop</option>
+              <option value="4">Gaming</option>
+            </select>
+          </div>
+          <input className="search-bar" type="text" placeholder="Search" />
 
-      <div className="search">
+          <button className="search-button">
+            <img className="search-icon" src={searchIcon} alt="Search" />
+          </button>
+        </div>
 
-        <input className="search-bar" type="text" placeholder="Search" />
+        <div className="directory">
+          <h5>Accessories</h5>
+          <p>Home>Shop Page>Accessories</p>
+        </div>
+        <div className="products-grid">
+          <div className="product-container">
+            <div className="product-image-container">
+              <img className="product-image" src="" />
+            </div>
+            <div className="product-name">Dell OptPlex 5060 Micro Core</div>
 
-        <button className="search-button">
-          <img className="search-icon" src="" alt="Search" />
-        </button>
+            <div className="product-rating-container">
+              <img className="product-rating-stars" src="" />
+              <div className="product-rating-count link-primary"> </div>
+            </div>
+
+            <div className="product-price">45,000FCFA</div>
+          </div>
+        </div>
+
+        <div className="update">
+          <span className="line"><hr /></span>
+          <h4>Stay Up To Date!</h4>
+          <span className="line"><hr /></span>
+        </div>
+        <div className="subscribe">
+          <h4>Join Our Newsletter</h4>
+          <div className="email">
+            <input type="text" placeholder="youremail@gmail.com" />
+            <button className="subs-btn">Subscribe</button>
+          </div>
+        </div>
+        
       </div>
 
       <Footer />
     </>
-  )
+  );
 }
